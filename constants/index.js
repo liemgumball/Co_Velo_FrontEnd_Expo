1e56cc2c0ab669f9f204fac1d579e9620a533bb3
liem_nguyen <liem1762001@gmail.com>
2023-02-10 13:57:30 +0700

    set dependencies

---
+++ constants/index.js
@@ -0,0 +1,1 @@
+import { COLORS, SHADOWS, SIZES, FONTS } from "./theme";
---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import { View, SafeAreaView, FlatList, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <Text>Hellokjhdaldhahdlakdhlakhdlakhdla</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
